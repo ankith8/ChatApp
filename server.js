9c5e25bda050443a73d8ebe242e0ceb5bf074ebf
ankith sadashiva <ankith8@gmail.com>
2016-09-14 00:08:15 +0530

    Added core files

---
+++ server.js
@@ -0,0 +1,43 @@
+var mongo = require('mongodb').MongoClient,
+ client = require('socket.io').listen(8080).sockets;
+
+mongo.connect("mongodb://127.0.0.1/chatapp",function (err,db) {
+  if(err) throw err;
+  client.on('connection',function (socket) {
+
+    var col = db.collection('messages'),
+        sendStatus = function(s){
+          socket.emit('status',s);
+        };
+
+    // Emit all messages
+    col.find().limit(100).sort({_id:1}).toArray(function(err,res){
+      if(err) throw err;
+      socket.emit('output',res);
+    });
+
+    // Wait for input
+    socket.on('input', function(data) {
+      var name = data.name,
+          message = data.message,
+          whitespacePattern = /^\s*$/;
+
+      if(whitespacePattern.test(name) || whitespacePattern.test(message)){
+        sendStatus('name and message is required');
+      }
+      else {
+        col.insert({name:name,message:message},function () {
+
+          // Emit latest messages to all clients
+          client.emit('output',[data]);
+
+          sendStatus({
+            message : "message sent",
+            clear : true
+          });
+        });
+      }
+
+    });
+  });
+});
